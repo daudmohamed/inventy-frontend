653fb84fca11d74dbda472dd7073f960035309d8
Daud Mohamed <daud.mohamed@fortedigital.no>
2023-08-30 15:38:36 +0200

    Define api and base rule in next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    AUTH0_BASE_URL: process.env.VERCEL_URL,
+    API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080',
+  },
+}
 
 module.exports = nextConfig
