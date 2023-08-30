fab652b668bc8fce97eb4c0643bc5dda858e2e8f
Daud Mohamed <daud.mohamed@fortedigital.no>
2023-08-30 17:44:10 +0200

    Improved API proxy

--- app/api/inventy/[slug]/route.ts
+++ app/api/inventy/[slug]/route.ts
@@ -1,39 +1,21 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest } from 'next/server'
 
-import Gets from './gets'
-import Posts from './posts'
-import { NextApiRequest, NextApiResponse } from 'next'
+import { handleAPI } from '@/app/api/inventy/[slug]/handleApi'
 
 type Parameters = { params: { slug: string } }
 
 export async function GET(request: NextRequest, context: Parameters) {
-  let route = context.params.slug
-  const handler = route && Gets.hasOwnProperty(route) && Gets[route]
-  try {
-    if (handler) {
-      return await handler(request, context)
-    } else {
-      return new Response(null, { status: 404 })
-    }
-  } catch (error) {
-    console.log(error)
-    // @ts-ignore
-    return new Response(error, { status: 500 })
-  }
+  return handleAPI(request, context)
 }
 
 export async function POST(request: NextRequest, context: Parameters) {
-  let route = context.params.slug
-  const handler = route && Posts.hasOwnProperty(route) && Posts[route]
-  try {
-    if (handler) {
-      return await handler(request, context)
-    } else {
-      return new Response(null, { status: 404 })
-    }
-  } catch (error) {
-    console.log(error)
-    // @ts-ignore
-    return new Response(error, { status: 500 })
-  }
+  return handleAPI(request, context)
+}
+
+export async function PUT(request: NextRequest, context: Parameters) {
+  return handleAPI(request, context)
+}
+
+export async function DELETE(request: NextRequest, context: Parameters) {
+  return handleAPI(request, context)
 }
